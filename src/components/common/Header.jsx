86feee8e5c3fbd3810b864f8a70e67478f50b4ce
Amaru333 <amrutesharun0599@gmail.com>
2025-08-03 13:16:35 -0400

    removed unnecessary texts from header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,6 @@
 import { Link } from 'react-router';
-import { useTheme } from '../ThemeContext';
 
 function Header() {
-  const { toggleTheme } = useTheme();
-
   return (
     <div className="bg-gradient-to-r from-blue-500 to-indigo-600 py-6 px-4 shadow-md">
       <h1 className="text-4xl font-bold text-white mb-4 tracking-tight">My Blog</h1>
@@ -15,11 +12,9 @@ function Header() {
           <li>
             <Link to="/posts">Blog posts</Link>
           </li>
-          <li>About</li>
           <li>
             <Link to="/contact">Contact</Link>
           </li>
-          <button onClick={toggleTheme}>Toggle Theme</button>
         </ul>
       </nav>
     </div>
